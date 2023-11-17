8c9fa7b336e521ec634c5d79a218133ed7566d2a
Illumincer <illumincer@gmail.com>
2023-11-17 10:21:01 +0300

    aaa

---
+++ request.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'inc/config.php';
+include 'inc/functions.php';
+include 'inc/anti.php';
+$q = $_GET['q'];
+
+if ($q == 'login') {
+  
+
+}elseif($q == 'logout'){
+
+
+}elseif($q == 'signup' && !isset($us['id'])){
+  
+    
+}elseif($q == "get-bank"){
+    
+}
