086c381bab206869ff31f162b170459bf51cb349
Illumincer <illumincer@gmail.com>
2023-11-21 13:34:38 +0300

    aaa

--- view/modules/footer.php
+++ view/modules/footer.php
@@ -361,7 +361,7 @@
             </div>
          </div>
       </div>
-      
+      <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
       <script>
       function openmodal(contentId, menuId) {
           $(".cdk-overlay-container").addClass("hidden");
