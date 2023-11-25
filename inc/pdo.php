e9497c8242f0f85b7d2faaf3f23b491374d54e20
Illumincer <illumincer@gmail.com>
2023-11-25 22:52:59 +0300

    aaa

--- inc/pdo.php
+++ inc/pdo.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+header('Content-Type: text/html; charset=utf-8');
+
+setlocale(LC_TIME, 'tr_TR.UTF-8', 'tr_TR', 'tr', 'turkish');
 $host = 'localhost'; // MySQL sunucu adresi
 $dbname = 'admin_migros'; // Veritabanı adı
 $user = 'admin_migros'; // MySQL kullanıcı adı
@@ -7,10 +10,8 @@ $password = '754_3Mtzo'; // MySQL şifre
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    // Hata modunu etkinleştir
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Bağlantı hatası durumunda hata mesajını göster
     die("Bağlantı hatası: " . $e->getMessage());
 }
 ?>
