87e4c0c45bad271c277e422829313b049e2295eb
Illumincer <illumincer@gmail.com>
2023-11-26 22:02:41 +0300

    aaa

--- view/modules/footer.php
+++ view/modules/footer.php
@@ -711,12 +711,12 @@
       </div>
 <script>
    function showCart() {
-  var cart = document.getElementById("cart");
+  var cart = document.getElementById("empty-cart");
   cart.classList.remove("hidden");
 }
 
 function hideCart() {
-  var cart = document.getElementById("cart");
+  var cart = document.getElementById("empty-cart");
   cart.classList.add("hidden");
 }
 
