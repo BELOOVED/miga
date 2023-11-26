b49fae3a08a7f19d1e67bd00ccd2e35471b002c5
Illumincer <illumincer@gmail.com>
2023-11-26 11:18:31 +0300

    aaa

--- view/modules/header.php
+++ view/modules/header.php
@@ -957,19 +957,9 @@ if ($existingUser) {
       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
       <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
       <style>
-      .hidden {
-  opacity: 0;
-  height: 0;
-  overflow: hidden;
-  transition: opacity 0.5s ease, height 0.5s ease; /* Animasyon süresi ve geçiş türü */
-}
-
-/* Eğer elementin görünür olması durumunu belirlemek istiyorsanız, bu stilin eklenmemesi gerekiyor. */
-.hidden.visible {
-  opacity: 1;
-  height: auto; /* veya istediğiniz bir yükseklik değeri */
-}
-
+         .hidden{
+         display: none !important;
+         }
       </style>
    </head>
    <body class="mat-typography">
