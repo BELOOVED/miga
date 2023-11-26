4b30ecf3d52ceca59fd2ec165c6a6c4781e0024c
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2023-11-26 21:47:53 +0300

    s

--- view/main.php
+++ view/main.php
@@ -7,13 +7,6 @@
             <div _ngcontent-cro-c416="" class="home-page-wrapper mdc-layout-grid__inner">
                 <?php
                 if ($detect->isMobile()) {?>
-                <div _ngcontent-cro-c416="" class="mdc-layout-grid__cell--span-12">
-                <div _ngcontent-cro-c416="">
-                    <sm-swiper-banner _ngcontent-cro-c416="" id="sm-home-main-top-banner" class="banners ng-star-inserted">
-                        <!---->
-                    </sm-swiper-banner>
-                    <!---->
-                </div>
                 <?php include "slider_gen.php"; ?>
                 <!---->
              
