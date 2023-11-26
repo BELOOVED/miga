a1ad3952d1db4c0043db8deff0db4e0d31177ce6
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2023-11-26 22:35:47 +0300

    s

--- view/slider_gen_web.php
+++ view/slider_gen_web.php
@@ -17,64 +17,23 @@ $stmt = $pdo->query("SELECT * FROM slider");
        </div>
        <!---->
         <div class="swiper-wrapper slick-slider" id="swiper-wrapper-9d019d3871f7b504" aria-live="off" style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
-        <?php
-        $slide_index = 0;
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo '<div data-swiper-slide-index="' . strval($slide_index) . '" class="swiper-slide" style="width: 1140px; role="group" aria-label="1 / 19">';
-                echo '<a href="' . $row['url'] . '">';
-                echo '<div class="image-container">';
-                echo '<img id="swiperImage' . $slide_index . '" alt="" class="lazy-image" data-src="https://external-content.duckduckgo.com/iu/?u=' . $row['image'] . '">';
-                echo '<img class="placeholder" src="/assets/images/banner_placeholder.webp" alt="">';
-                echo '</div>';
-                echo '</a>';
-                echo '</div>';
-                $slide_index += 1;
-            }
-        ?>
+            <?php
+                $slide_index = 0;
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    if ($slide_index == 0 ){
+                        echo '<div data-swiper-slide-index="' . strval($slide_index) . '" class="swiper-slide swiper-slide-active" style="width: 1140px; role="group" aria-label="1 / 19">';
+                        echo '<a href="' . $row['url'] . '"><img alt="" class="swiper-lazy swiper-lazy-loaded" src="https://external-content.duckduckgo.com/iu/?u=' . $row['image'] . '"></a>';
+                        echo '</div>';
+                    }else{
+                        echo '<div data-swiper-slide-index="' . strval($slide_index) . '" class="swiper-slide swiper-slide-next" style="width: 1140px; role="group" aria-label="1 / 19">';
+                        echo '<a href="' . $row['url'] . '"><img alt="" class="swiper-lazy swiper-lazy-loaded" src="https://external-content.duckduckgo.com/iu/?u=' . $row['image'] . '"></a>';
+                        echo '</div>';
+                    }
+                    $slide_index += 1;
+                }
+            ?>
+          <!----><!---->
        </div>
-       <style>
-        .image-container {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-        }
-
-        .lazy-image {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            transition: opacity 0.3s ease;
-            opacity: 0;
-        }
-
-        .lazy-image.loaded {
-            opacity: 1;
-        }
-
-        .placeholder {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-        }
-        </style>
-
-        <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var lazyImages = document.querySelectorAll('.lazy-image');
-
-            lazyImages.forEach(function (lazyImage) {
-            lazyImage.addEventListener('load', function () {
-                lazyImage.classList.add('loaded');
-            });
-            });
-        });
-        </script>
-
        <!----><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
     </swiper>
     <!---->
