ba4143c91ba48508c4025de1bebb63ed808b5a13
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2023-11-27 21:06:35 +0300

    sd

--- zort.php
+++ zort.php
@@ -4,10 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$veri = slider_update();
-
-foreach ($variable as $veri["data"]) {
-    echo($variable);
-}
+$veri = slider_update()["data"];
+echo $veri;
 
 ?>
