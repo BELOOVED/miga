d9df8f681b1a58dae9b55d092fda6edb3960da52
Illumincer <illumincer@gmail.com>
2023-12-01 16:58:22 +0300

    aaaa

--- view/kategoriler.php
+++ view/kategoriler.php
@@ -2,35 +2,35 @@
    <router-outlet _ngcontent-cro-c368=""></router-outlet>
    <sm-categories _nghost-cro-c423="" class="ng-star-inserted">
       <div _ngcontent-cro-c423="" class="categories-wrapper">
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/indirimli-urunler" style="background-color: rgb(255, 232, 225);">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/indirimli-urunler/1" style="background-color: rgb(255, 232, 225);">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/73630/tum_indirimler_logo_3x-18425a.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Tüm İndirimli Ürünler</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/telefen-ve-aksesuarlari/1">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/telefon-ve-aksesuarlari/1317">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/1317/1.telefon-ve-aksesuarlari-3b6d68.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Telefon ve Aksesuarları</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/bilgisayar-ve-aksesuarlari/2">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/bilgisayar-ve-aksesuarlari/1315">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/1315/2.bilgisayar-ve-aksesuarlari-11ef63.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Bilgisayar ve Aksesuarları</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/beyaz-esya/3">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/beyaz-esya/1313">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/1313/3.beyaz-esya-7532dd.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Beyaz Eşya</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/elektrikli-ev-aletleri/4">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/elektrikli-ev-aletleri/1314">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/1314/4.elektrikli-ev-aletleri-d27117.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Elektrikli Ev Aletleri</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/oyun-konsollari/5">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/oyun-konsollari/10964">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/10964/5.oyun-konsollari-f054ff.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Oyun Konsolları</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/goruntu-ve-ses-sistemleri/6">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/goruntu-ve-ses-sistemleri/1316">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/1316/6.goruntu-ve-ses-sistemleri-d645bc.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Görüntü ve Ses Sistemleri</div>
          </a>
-         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/isitma-sogutma/7">
+         <a _ngcontent-cro-c423="" class="category-card ng-star-inserted" ngx-ql="" href="/urunler/isitma-sogutma/70185">
             <img _ngcontent-cro-c423="" class="image" src="https://images.migrosone.com/elektronik/category/list/70185/7.isitma-sogutma-553e93.png">
             <div _ngcontent-cro-c423="" class="category-name mat-caption text-color-black">Isıtma, Soğutma</div>
          </a>
