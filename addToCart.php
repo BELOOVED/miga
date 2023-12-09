588335f800dc7930fbd2fb96681562a92b1c950c
Illumincer <illumincer@gmail.com>
2023-12-09 16:40:31 +0300

    aaa

--- addToCart.php
+++ addToCart.php
@@ -1,12 +1,17 @@
 <?php
+// addToCart.php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // AJAX isteği üzerinden gelen verileri al
     $productId = $_POST["productId"];
 
-    setcookie("cart_item_$productId", "1", time() + 86400, "/");
+    // Cookie oluştur (örnek: 1 saat süreyle geçerli)
+    setcookie("cart_item_$productId", "1", time() + 3600, "/");
 
+    // İşlem başarılı olduysa bir cevap gönder
     echo "Ürün sepete eklendi!";
 } else {
+    // Hatalı bir istek geldiyse hata mesajı gönder
     header("HTTP/1.1 400 Bad Request");
     echo "Hatalı istek!";
 }
