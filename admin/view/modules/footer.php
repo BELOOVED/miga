6d3f6416c58a05c9ee1c76910bd5eecf2a892824
Illumincer <illumincer@gmail.com>
2023-12-23 12:05:23 +0300

    aaa

--- admin/view/modules/footer.php
+++ admin/view/modules/footer.php
@@ -6,7 +6,7 @@
 	<footer class="footer">
       <div class="container">
         <div class="text-center">
-          Copyright © 2018 Dashtreme Admin
+          Copyright © 2023 Migros Panel
         </div>
       </div>
     </footer>
@@ -77,7 +77,26 @@
   <script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
   <script src="assets/plugins/jquery-knob/excanvas.js"></script>
   <script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
-    
+     <!-- simplebar js -->
+  <script src="assets/plugins/simplebar/js/simplebar.js"></script>
+  <!-- sidebar-menu js -->
+  <script src="assets/js/sidebar-menu.js"></script>
+  
+  <!-- Custom scripts -->
+  <script src="assets/js/app-script.js"></script>
+
+  <!--Data Tables js-->
+  <script src="assets/plugins/bootstrap-datatable/js/jquery.dataTables.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/dataTables.bootstrap4.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/dataTables.buttons.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/buttons.bootstrap4.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/jszip.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/pdfmake.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/vfs_fonts.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/buttons.html5.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/buttons.print.min.js"></script>
+  <script src="assets/plugins/bootstrap-datatable/js/buttons.colVis.min.js"></script>
+
     <script>
         $(function() {
             $(".knob").knob();
@@ -86,6 +105,22 @@
   <!-- Index js -->
   <script src="assets/js/index.js"></script>
 
-  
+  <script>
+     $(document).ready(function() {
+      //Default data table
+       $('#default-datatable').DataTable();
+
+
+       var table = $('#example').DataTable( {
+        lengthChange: false,
+        buttons: [ 'copy', 'excel', 'pdf', 'print', 'colvis' ]
+      } );
+ 
+     table.buttons().container()
+        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
+      
+      } );
+
+    </script>
 </body>
 </html>
