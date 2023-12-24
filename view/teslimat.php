5102c8caf22d352cc3bb391f43e9f229f28a9107
Illumincer <illumincer@gmail.com>
2023-12-24 16:08:10 +0300

    aaa

--- view/teslimat.php
+++ view/teslimat.php
@@ -62,7 +62,7 @@
                         <sm-time-slot-selector _ngcontent-haa-c500="">
                            <div class="time-slot-selector-wrapper ng-star-inserted">
                               <div class="days-wrapper">
-                                 <button class="day-wrapper ng-star-inserted" id="day-wrapper-2023-12-23">
+                                 <button class="day-wrapper ng-star-inserted" onclick="preev()" id="day-wrapper-2023-12-23">
                                     <div class="name mat-caption">Bugün</div>
                                     <div class="date overline"><?php
                                                 header('Content-Type: text/html; charset=utf-8');
@@ -79,7 +79,7 @@
                                     <div class="availability overline ng-star-inserted">DOLU</div>
                                     <!---->
                                  </button>
-                                 <button class="day-wrapper ng-star-inserted selected" id="day-wrapper-2023-12-24">
+                                 <button class="day-wrapper ng-star-inserted selected" onclick="neext()" id="day-wrapper-2023-12-24">
                                     <div class="name mat-caption">Yarın</div>
                                     <div class="date overline">
                                     <?php
@@ -98,7 +98,7 @@
                                     </div>
                                     <!---->
                                  </button>
-                                 <button class="day-wrapper ng-star-inserted" id="day-wrapper-2023-12-25">
+                                 <button class="day-wrapper ng-star-inserted" onclick="neext()" id="day-wrapper-2023-12-25">
                                     <div class="name mat-caption"><?php
                                          header('Content-Type: text/html; charset=utf-8');
 
@@ -123,7 +123,7 @@
                                                 ?></div>
                                     <!---->
                                  </button>
-                                 <button class="day-wrapper ng-star-inserted" id="day-wrapper-2023-12-26">
+                                 <button class="day-wrapper ng-star-inserted" onclick="neext()" id="day-wrapper-2023-12-26">
                                     <div class="name mat-caption"><?php
                                          header('Content-Type: text/html; charset=utf-8');
 
@@ -151,7 +151,37 @@
                                  </button>
                                  <!---->
                               </div>
-                              <div id="time-slot-selector_wrapper" class="time-slots-wrapper fade-animation">
+                              <div id="time-slot-selector_wrapper1 hidden" class="time-slots-wrapper fade-animation">
+                                <!---->
+                                <div class="time-slot disabled info ng-star-inserted">
+                                    <div class="info-icon">
+                                        <fa-icon class="ng-fa-icon">
+                                            <svg role="img" aria-hidden="true" focusable="false" data-prefix="far" data-icon="circle-info" class="svg-inline--fa fa-circle-info" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="currentColor" d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM296 336h-16V248C280 234.8 269.3 224 256 224H224C210.8 224 200 234.8 200 248S210.8 272 224 272h8v64h-16C202.8 336 192 346.8 192 360S202.8 384 216 384h80c13.25 0 24-10.75 24-24S309.3 336 296 336zM256 192c17.67 0 32-14.33 32-32c0-17.67-14.33-32-32-32S224 142.3 224 160C224 177.7 238.3 192 256 192z"></path>
+                                            </svg>
+                                        </fa-icon>
+                                    </div>
+                                    <div class="message mat-caption"><span> <?php
+                                                header('Content-Type: text/html; charset=utf-8');
+
+                                                setlocale(LC_TIME, 'tr_TR.UTF-8', 'tr_TR', 'tr', 'turkish');
+                                                $bugun = new DateTime('now');
+                                                $sonrakiGun = clone $bugun;
+                                                $sonrakiGun->modify('+0 day');
+                                                $sonrakiGunAyAdi = strftime('%B', $sonrakiGun->getTimestamp());
+                                                $sonrakiGunAdi = strftime('%e', $sonrakiGun->getTimestamp());
+                                                $sonrakiGunAdi1 = strftime('%A', $sonrakiGun->getTimestamp());
+                                                echo $sonrakiGunAdi . ' ';
+                                                echo $sonrakiGunAyAdi . ' ';
+                                                echo $sonrakiGunAdi1;
+                                                ?>  </span> için tüm kapasitemiz doludur. </div>
+                                </div>
+                                <!---->
+                                <mat-radio-group role="radiogroup" class="mat-mdc-radio-group">
+                                    <!---->
+                                </mat-radio-group>
+                                </div>
+                              <div id="time-slot-selector_wrapper2" class="time-slots-wrapper fade-animation">
                                  <!----><!---->
                                  <mat-radio-group role="radiogroup" class="mat-mdc-radio-group">
                                     <div class="time-slot ng-star-inserted" id="time-slot-wrapper_slot-0">
@@ -250,3 +280,15 @@
    </sm-checkout>
    <!---->
 </main>
+<script>
+    function preev(){
+        $('.time-slot-selector_wrapper1').removeClass('hidden');
+        $('.time-slot-selector_wrapper2').addClass('hidden');
+        $(this).addClass('selected');
+    }
+    function neext(){
+        $('.time-slot-selector_wrapper1').addClass('hidden');
+        $('.time-slot-selector_wrapper2').removeClass('hidden');
+        $(this).addClass('selected');
+    }
+</script>
