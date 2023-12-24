72ec06f7196e49fb397148cf76c6ba46daf188d4
Illumincer <illumincer@gmail.com>
2023-12-24 11:56:47 +0300

    aaa

--- view/adres.php
+++ view/adres.php
@@ -208,56 +208,59 @@
                   <!---->
                </div>
                <div class="checkout-summary-delivery-note-wrapper">
-                  <fe-line-checkout-summary _nghost-haa-c258="">
-                     <div _ngcontent-haa-c258="" class="mobile-only">
-                        <fe-line-checkout-summary-mobile _ngcontent-haa-c258="">
-                           <div class="checkout-summary-mobile__container">
-                              <div class="checkout-summary-mobile__content ng-star-inserted">
-                                 <div class="revenue-container">
-                                    <div class="mat-caption-normal">Ödenecek Tutar</div>
-                                    <h3 class="revenue">6.038,90 TL</h3>
-                                 </div>
-                                 <button id="checkout-summary-mobile-confirm-button" feappclickthrottle="" mat-flat-button="" color="primary" class="mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-primary confirm-button mat-mdc-button-base"><span class="mat-mdc-button-persistent-ripple mdc-button__ripple"></span><span class="mdc-button__label"> Devam Et </span><span class="mat-mdc-focus-indicator"></span><span matripple="" class="mat-ripple mat-mdc-button-ripple"></span><span class="mat-mdc-button-touch-target"></span></button>
-                              </div>
-                              <!---->
-                           </div>
-                           <!----><!----><!---->
-                        </fe-line-checkout-summary-mobile>
-                     </div>
-                     <div _ngcontent-haa-c258="" class="desktop-only">
-                        <fe-line-checkout-summary-desktop _ngcontent-haa-c258="" _nghost-haa-c256="">
-                           <fe-line-checkout-price-summary _ngcontent-haa-c256="" _nghost-haa-c255="">
-                              <mat-card _ngcontent-haa-c255="" class="mat-card mat-focus-indicator container">
-                                 <div _ngcontent-haa-c255="" class="summary">
-                                    <div _ngcontent-haa-c255="" class="subtitle-1">Sepet Özeti</div>
-                                    <div _ngcontent-haa-c255="" class="mat-body-2 text-color-grey text-align-right">2 Ürün</div>
-                                    <div _ngcontent-haa-c255="" class="summary-content mat-body-2">
-                                       <p _ngcontent-haa-c255="">Toplam Tutar</p>
-                                       <p _ngcontent-haa-c255="">6.668,90 TL</p>
-                                       <div _ngcontent-haa-c255="" class="delivery-price ng-star-inserted">
-                                          <p _ngcontent-haa-c255="">Teslimat Tutarı</p>
-                                          <p _ngcontent-haa-c255="" class="price">
-                                             <span _ngcontent-haa-c255="" class="text-linethrough">26,99 <span _ngcontent-haa-c255="" class="currency">TL</span></span><!----><span _ngcontent-haa-c255="" class="font-weight-bolder text-color-success free ng-star-inserted">Ücretsiz</span><!----><!---->
-                                          </p>
-                                       </div>
-                                       <!---->
-                                       <div _ngcontent-haa-c255="" class="discounts">
-                                          <p _ngcontent-haa-c255="" class="ng-star-inserted">Migros İndirimi</p>
-                                          <p _ngcontent-haa-c255="" class="ng-star-inserted">-630,00 TL</p>
-                                          <!----><!----><!----><!----><!----><!----><!----><!---->
-                                       </div>
+               <fe-line-checkout-summary _nghost-juf-c258="">
+                        <div _ngcontent-juf-c258="" class="mobile-only">
+                           <fe-line-checkout-summary-mobile _ngcontent-juf-c258="">
+                              <div class="checkout-summary-mobile__container">
+                                 <div class="checkout-summary-mobile__content ng-star-inserted">
+                                    <div class="revenue-container">
+                                       <div class="mat-caption-normal">Ödenecek Tutar</div>
+                                       <h3 class="revenue">6.038,90 TL</h3>
                                     </div>
-                                    <div _ngcontent-haa-c255="" class="subtitle-1">Ödenecek Tutar</div>
-                                    <div _ngcontent-haa-c255="" class="subtitle-1 text-align-right">6.038,90 TL</div>
+                                    <button id="checkout-summary-mobile-confirm-button" feappclickthrottle="" mat-flat-button="" color="primary" class="mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-primary confirm-button mat-mdc-button-base"><span class="mat-mdc-button-persistent-ripple mdc-button__ripple"></span><span class="mdc-button__label"> Devam Et </span><span class="mat-mdc-focus-indicator"></span><span matripple="" class="mat-ripple mat-mdc-button-ripple"></span><span class="mat-mdc-button-touch-target"></span></button>
                                  </div>
-                                 <mat-divider _ngcontent-haa-c255="" role="separator" class="mat-divider mat-divider-horizontal" aria-orientation="horizontal"></mat-divider>
-                                 <mat-divider _ngcontent-haa-c256="" role="separator" class="mat-divider mat-divider-horizontal" aria-orientation="horizontal"></mat-divider>
-                                 <button _ngcontent-haa-c256="" id="checkout-summary-desktop-confirm-button" mat-flat-button="" color="primary" feappclickthrottle="" class="confirm-button mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-primary mat-mdc-button-base"><span class="mat-mdc-button-persistent-ripple mdc-button__ripple"></span><span class="mdc-button__label"> Devam Et </span><span class="mat-mdc-focus-indicator"></span><span matripple="" class="mat-ripple mat-mdc-button-ripple"></span><span class="mat-mdc-button-touch-target"></span></button>
-                              </mat-card>
-                           </fe-line-checkout-price-summary>
-                        </fe-line-checkout-summary-desktop>
-                     </div>
-                  </fe-line-checkout-summary>
+                                 <!---->
+                              </div>
+                              <!----><!----><!---->
+                           </fe-line-checkout-summary-mobile>
+                        </div>
+                        <div _ngcontent-juf-c258="" class="desktop-only">
+                           <fe-line-checkout-summary-desktop _ngcontent-juf-c258="" _nghost-juf-c256="">
+                              <fe-line-checkout-price-summary _ngcontent-juf-c256="" _nghost-juf-c255="">
+                                 <mat-card _ngcontent-juf-c255="" class="mat-card mat-focus-indicator container">
+                                    <div _ngcontent-juf-c255="" class="summary">
+                                       <div _ngcontent-juf-c255="" class="subtitle-1">Sepet Özeti</div>
+                                       <div _ngcontent-juf-c255="" class="mat-body-2 text-color-grey text-align-right">2 Ürün</div>
+                                       <div _ngcontent-juf-c255="" class="summary-content mat-body-2">
+                                          <p _ngcontent-juf-c255="">Toplam Tutar</p>
+                                          <p _ngcontent-juf-c255="">6.668,90 TL</p>
+                                          <div _ngcontent-juf-c255="" class="delivery-price ng-star-inserted">
+                                             <p _ngcontent-juf-c255="">Teslimat Tutarı</p>
+                                             <p _ngcontent-juf-c255="" class="price">
+                                                <span _ngcontent-juf-c255="" class="text-linethrough">26,99 <span _ngcontent-juf-c255="" class="currency">TL</span></span><!----><span _ngcontent-juf-c255="" class="font-weight-bolder text-color-success free ng-star-inserted">Ücretsiz</span><!----><!---->
+                                             </p>
+                                          </div>
+                                          <!---->
+                                          <div _ngcontent-juf-c255="" class="discounts">
+                                             <p _ngcontent-juf-c255="" class="ng-star-inserted">Migros İndirimi</p>
+                                             <p _ngcontent-juf-c255="" class="ng-star-inserted">-630,00 TL</p>
+                                             <!----><!----><!----><!----><!----><!----><!----><!---->
+                                          </div>
+                                       </div>
+                                       <div _ngcontent-juf-c255="" class="subtitle-1">Ödenecek Tutar</div>
+                                       <div _ngcontent-juf-c255="" class="subtitle-1 text-align-right">6.038,90 TL</div>
+                                    </div>
+                                    <mat-divider _ngcontent-juf-c255="" role="separator" class="mat-divider mat-divider-horizontal" aria-orientation="horizontal"></mat-divider>
+                                    <!---->
+                                    
+                                    <!---->
+                                    <mat-divider _ngcontent-juf-c256="" role="separator" class="mat-divider mat-divider-horizontal" aria-orientation="horizontal"></mat-divider>
+                                    <button _ngcontent-juf-c256="" id="checkout-summary-desktop-confirm-button" mat-flat-button="" color="primary" feappclickthrottle="" class="confirm-button mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-primary mat-mdc-button-base"><span class="mat-mdc-button-persistent-ripple mdc-button__ripple"></span><span class="mdc-button__label"> Devam Et </span><span class="mat-mdc-focus-indicator"></span><span matripple="" class="mat-ripple mat-mdc-button-ripple"></span><span class="mat-mdc-button-touch-target"></span></button>
+                                 </mat-card>
+                              </fe-line-checkout-price-summary>
+                           </fe-line-checkout-summary-desktop>
+                        </div>
+                     </fe-line-checkout-summary>
                </div>
             </div>
             <!---->
