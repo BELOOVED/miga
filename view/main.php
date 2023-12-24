73f2b2198b4572b0ac5867fcf0bf4fc1ca535fb9
Illumincer <illumincer@gmail.com>
2023-12-24 22:05:25 +0300

    aaa

--- view/main.php
+++ view/main.php
@@ -140,7 +140,7 @@
                                     <!---->
                                     <sm-product-actions id="actions" class="actions" _nghost-cro-c292="">
                                         <!---->
-                                        <fa-icon _ngcontent-cro-c292="" class="ng-fa-icon add-to-cart-button" id="product-actions-add-to-cart-button--papilla-cift-tarafli-balik-tavasi-28-cm-p-1e8a802">
+                                        <fa-icon onclick="addToCart(<?=$urun['id'];?>)" _ngcontent-cro-c292="" class="ng-fa-icon add-to-cart-button" id="product-actions-add-to-cart-button--papilla-cift-tarafli-balik-tavasi-28-cm-p-1e8a802">
                                             <svg role="img" aria-hidden="true" focusable="false" data-prefix="far" data-icon="plus" class="svg-inline--fa fa-plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                                 <path fill="currentColor" d="M432 256C432 269.3 421.3 280 408 280h-160v160c0 13.25-10.75 24.01-24 24.01S200 453.3 200 440v-160h-160c-13.25 0-24-10.74-24-23.99C16 242.8 26.75 232 40 232h160v-160c0-13.25 10.75-23.99 24-23.99S248 58.75 248 72v160h160C421.3 232 432 242.8 432 256z"></path>
                                             </svg>
--- view/urunler.php
+++ view/urunler.php
@@ -414,7 +414,7 @@ if ($detect->isMobile()) {?>
                                         <!---->
                                         <sm-product-actions id="actions" class="actions" _nghost-nyw-c292="">
                                             <!---->
-                                            <fa-icon _ngcontent-nyw-c292="" class="ng-fa-icon add-to-cart-button ng-star-inserted" id="product-actions-add-to-cart-button--xiaomi-redmi-buds-3-bluetooth-kulaklik-beyaz-p-255e133">
+                                            <fa-icon onclick="addToCart(<?=$urun['id'];?>)" _ngcontent-nyw-c292="" class="ng-fa-icon add-to-cart-button ng-star-inserted" id="product-actions-add-to-cart-button--xiaomi-redmi-buds-3-bluetooth-kulaklik-beyaz-p-255e133">
                                             <svg role="img" aria-hidden="true" focusable="false" data-prefix="far" data-icon="plus" class="svg-inline--fa fa-plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                                 <path fill="currentColor" d="M432 256C432 269.3 421.3 280 408 280h-160v160c0 13.25-10.75 24.01-24 24.01S200 453.3 200 440v-160h-160c-13.25 0-24-10.74-24-23.99C16 242.8 26.75 232 40 232h160v-160c0-13.25 10.75-23.99 24-23.99S248 58.75 248 72v160h160C421.3 232 432 242.8 432 256z"></path>
                                             </svg>
