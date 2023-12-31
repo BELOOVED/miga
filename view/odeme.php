d9ca964fd8de29f4eba558ca9199b9650db7ed07
Illumincer <illumincer@gmail.com>
2023-12-31 22:38:38 +0300

    aaa

--- view/odeme.php
+++ view/odeme.php
@@ -68,7 +68,13 @@
                               </div>
                               <div class="line"></div>
                            </div>
-                           <div class="step-content">
+                           <div class="step-content" style="
+                           
+                           position: relative;
+                           margin-top: 40px;
+                           right: 5px;
+                           width: 100% !important;
+                           ">
                               <div class="step-title"> Teslimat Adresin </div>
                               <div class="content">
                                  <sm-delivery-info class="ng-star-inserted">
