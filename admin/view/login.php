9e24bbf2a3392a402bf6784e1b1be05e5dcbfe9d
Illumincer <illumincer@gmail.com>
2023-12-31 13:52:06 +0300

    aaa

--- admin/view/login.php
+++ admin/view/login.php
@@ -34,7 +34,7 @@
 		<div class="card-body">
 		 <div class="card-content p-2">
 		 	<div class="text-center">
-		 		<img src="assets/images/logo-icon.png" alt="logo icon">
+		 		<img src="assets/images/logo-icon.png" style="width: 100px" alt="logo icon">
 		 	</div>
 		  <div class="card-title text-uppercase text-center py-3">Sign In</div>
 		    <form>
