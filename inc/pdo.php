ba97e64efbf4921b0715d2556a5e1e710454e786
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2023-12-31 15:11:56 +0300

    asd

--- inc/pdo.php
+++ inc/pdo.php
@@ -2,11 +2,11 @@
 ob_start();
 session_start();
 date_default_timezone_set('Europe/Istanbul');
-// error_reporting(0);
+error_reporting(0);
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
 
 $host = 'localhost';
 $dbname = 'admin_migros';
