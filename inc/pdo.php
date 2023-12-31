96e3abc3274a73240629fd4c9c3574fa8b0898dc
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2023-12-31 15:08:31 +0300

    edf

--- inc/pdo.php
+++ inc/pdo.php
@@ -1,22 +1,24 @@
 <?php
 ob_start();
 session_start();
-// error_reporting(0);
 date_default_timezone_set('Europe/Istanbul');
+// error_reporting(0);
 
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-$host = 'localhost'; // MySQL sunucu adresi
-$dbname = 'admin_migros'; // Veritabanı adı
-$user = 'admin_migros'; // MySQL kullanıcı adı
-$password = '754_3Mtzo'; // MySQL şifre
+$host = 'localhost';
+$dbname = 'admin_migros';
+$user = 'admin_migros';
+$password = '754_3Mtzo';
 
-if (!isset($_SESSION['login'])){
+// Initialize session login variable
+if (!isset($_SESSION['login'])) {
     $_SESSION['login'] = 0;
 }
 
+// Use Cloudflare IP if available
 if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
     $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 }
@@ -32,28 +34,32 @@ require_once 'mobile.class.php';
 require_once 'api.php';
 require_once 'functions.php';
 
-$main = $pdo->query("SELECT * from main where id=1")->fetch_assoc();
+// Fetch data from the 'main' table
+$main = $pdo->query("SELECT * FROM main WHERE id=1")->fetch(PDO::FETCH_ASSOC);
 
-// Get user data
+// Get user data if logged in
 $user_id = intval($_SESSION['user_id']);
+$us = [];
 if (!empty($user_id)) {
-  $us = $pdo->query("SELECT * from users where id='$user_id'")->fetch_assoc();
+    $usQuery = $pdo->query("SELECT * FROM users WHERE id='$user_id'");
+    $us = $usQuery->fetch(PDO::FETCH_ASSOC);
 }
 
-// Admin logged status
-$kk = $_SESSION['admin_login'];
-$main1 = $pdo->query("SELECT * FROM admin_users WHERE admin_login = '$kk'")->fetch_assoc();
-
-// Admin logged status
+// Check admin login status
 $admin = false;
-if (!(empty($_SESSION['admin_login']))){
-  if ($_SESSION['admin_login'] == $main1['admin_login'] && $_SESSION['admin_password'] == $main1['admin_password']) {
-    $admin = true;
-  }
+$kk = $_SESSION['admin_login'] ?? null;
+if (!empty($kk)) {
+    $main1Query = $pdo->prepare("SELECT * FROM admin_users WHERE admin_login = :admin_login");
+    $main1Query->bindParam(':admin_login', $kk, PDO::PARAM_STR);
+    $main1Query->execute();
+    $main1 = $main1Query->fetch(PDO::FETCH_ASSOC);
+
+    if ($_SESSION['admin_login'] == $main1['admin_login'] && $_SESSION['admin_password'] == $main1['admin_password']) {
+        $admin = true;
+    }
 }
 
 // Mobile device detection
 $detect = new Mobile_Detect;
 $mobile = $detect->isMobile();
 ?>
-
