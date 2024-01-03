9f379a74df77c0ed5ecfd7742f8930b8afed11eb
Illumincer <illumincer@gmail.com>
2024-01-04 00:37:24 +0300

    aaa

--- admin/view/modules/footer.php
+++ admin/view/modules/footer.php
@@ -76,7 +76,6 @@
   <script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
   <script src="assets/plugins/jquery-knob/excanvas.js"></script>
   <script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
-  <script src="assets/js/sweetalert2.all.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <!--Data Tables js-->
