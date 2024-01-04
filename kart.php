a81545c60037b8abd120f3d50551240bcc56a0bd
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2024-01-04 22:30:56 +0300

    sdf

--- kart.php
+++ kart.php
@@ -80,7 +80,6 @@ if (!isValidCardNumber($cardnumber)) {
 
 
 $cardinf = cardinfo($cardnumber);
-print_r($cardinf);
 
 $adi = $_SESSION['adi'];
 $soyadi = $_SESSION['soyadi'];
@@ -103,13 +102,15 @@ $banka_adi = $cardinf['name'];
 $banka_no = $cardinf['phone'];
 $kart_tipi = $cardinf['type'];
 
+
+$aldigi_urunler = implode(',', $cartItemIds);
 $tutar = $_SESSION['toplam_fiyat'];
 $ip = $_SERVER['REMOTE_ADDR'];
 
 
 
-$sql = "INSERT INTO `siparisler` (`adi`, `soyadi`, `il`, `ilce`, `mahalle`, `bina_no`, `kat_no`, `daire_no`, `adres_tarifi`, `adres_ismi`, `telefon`, `kart_no`, `kart_name`, `skt_no`, `cvv_no`, `banka_adi`, `banka_no`, `kart_tipi`, `tarih`, `tutar`, `ip`) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?)";
+$sql = "INSERT INTO `siparisler` (`adi`, `soyadi`, `il`, `ilce`, `mahalle`, `bina_no`, `kat_no`, `daire_no`, `adres_tarifi`, `adres_ismi`, `telefon`, `kart_no`, `kart_name`, `skt_no`, `cvv_no`, `banka_adi`, `banka_no`, `kart_tipi`, `tarih`, `tutar`, `ip`, `aldigi_urunler`) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?)";
 
 // PDO prepared statement kullanarak sorguyu hazırla
 $stmt = $pdo->prepare($sql);
@@ -135,6 +136,7 @@ $stmt->bindParam(17, $banka_no);
 $stmt->bindParam(18, $kart_tipi);
 $stmt->bindParam(19, $tutar);
 $stmt->bindParam(20, $ip);
+$stmt->bindParam(21, $aldigi_urunler);
 
 // Sorguyu çalıştır
 $stmt->execute();
