b906fc1aa9a17398213cddf48ae94896440fa9b3
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2024-01-04 22:09:17 +0300

    sda

--- kart.php
+++ kart.php
@@ -1,6 +1,10 @@
 <?php
 include 'inc/pdo.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 function isValidCardNumber($cardNumber) {
     if (empty($cardNumber) || !is_numeric($cardNumber) || $cardNumber <= 0) {
         return false;
