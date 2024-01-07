69ac816a1dd384af611b429b8872cb3c9d7ed47c
Illumincer <illumincer@gmail.com>
2024-01-07 22:40:01 +0300

    aaa

--- admin/view/siteyarlari.php
+++ admin/view/siteyarlari.php
@@ -3,27 +3,7 @@
       <!-- Breadcrumb-->
       <div class="row pt-2 pb-2">
          <div class="col-sm-9">
-            <h4 class="page-title">Form Layouts</h4>
-            <ol class="breadcrumb">
-               <li class="breadcrumb-item"><a href="javaScript:void();">Dashtreme</a></li>
-               <li class="breadcrumb-item"><a href="javaScript:void();">Forms</a></li>
-               <li class="breadcrumb-item active" aria-current="page">Form Layouts</li>
-            </ol>
-         </div>
-         <div class="col-sm-3">
-            <div class="btn-group float-sm-right">
-               <button type="button" class="btn btn-light waves-effect waves-light"><i class="fa fa-cog mr-1"></i> Setting</button>
-               <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split waves-effect waves-light" data-toggle="dropdown">
-               <span class="caret"></span>
-               </button>
-               <div class="dropdown-menu">
-                  <a href="javaScript:void();" class="dropdown-item">Action</a>
-                  <a href="javaScript:void();" class="dropdown-item">Another action</a>
-                  <a href="javaScript:void();" class="dropdown-item">Something else here</a>
-                  <div class="dropdown-divider"></div>
-                  <a href="javaScript:void();" class="dropdown-item">Separated link</a>
-               </div>
-            </div>
+            <h4 class="page-title">Site Ayarları</h4>
          </div>
       </div>
       <!-- End Breadcrumb-->
@@ -43,20 +23,12 @@
                         <input type="text" class="form-control" id="input-2" placeholder="Admin Panel Yolu">
                      </div>
                      <div class="form-group">
-                        <label for="input-3">Site Aç/Kapa</label>
-                        <input type="text" class="form-control" id="input-3" placeholder="Site Aç/Kapa">
-                     </div>
-                     <div class="form-group">
                         <label for="input-4">Telegram Bot Token</label>
                         <input type="text" class="form-control" id="input-4" placeholder="Telegram Bot Token">
                      </div>
                      <div class="form-group">
                         <label for="input-5">Telegram Chat ID</label>
                         <input type="text" class="form-control" id="input-5" placeholder="Telegram Chat ID">
-                     </div>
-                     <div class="form-group">
-                        <label for="input-5"> IBAN / KrediKartı</label>
-                        <input type="text" class="form-control" id="input-5" placeholder="KrediKartı İle Ödeme Alınıyor">
                      </div>
                      <div class="form-group">
                         <button type="submit" class="btn btn-light px-5"><i class="icon-lock"></i> Kaydet</button>
---
+++ admin/view/urunekle.php
@@ -0,0 +1,73 @@
+<div class="content-wrapper">
+   <div class="container-fluid">
+      <!-- Breadcrumb-->
+      <div class="row pt-2 pb-2">
+         <div class="col-sm-9">
+            <h4 class="page-title">Ürün Ekle</h4>
+         </div>
+      </div>
+      <!-- End Breadcrumb-->
+      <div class="row">
+         <div class="col-lg-12">
+            <div class="card">
+               <div class="card-body">
+                  <div class="card-title">Ürün Ekle</div>
+                  <hr>
+                  <form>
+                     <div class="form-group">
+                        <label for="input-1">Ürün Adı</label>
+                        <input type="text" class="form-control" id="input-1" placeholder="Ürün Adını Giriniz">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-2">Ürün Marka</label>
+                        <input type="text" class="form-control" id="input-2" placeholder="Ürün Markasını Giriniz">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-4">Ürün Fiyat</label>
+                        <input type="text" class="form-control" id="input-4" placeholder="Ürün Fiyatını Giriniz">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-5">Ürün Resim 1</label>
+                        <input type="file" class="form-control" id="input-5" placeholder="Ürünün İlk Resmi">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-5">Ürün Resim 2</label>
+                        <input type="file" class="form-control" id="input-5" placeholder="Ürünün İkinci Resmi">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-5">Ürün Resim 3</label>
+                        <input type="text" class="form-control" id="input-5" placeholder="Ürünün Üçüncü Resmi">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-5">Ürün Resim 4</label>
+                        <input type="file" class="form-control" id="input-5" placeholder="Ürünün Dördüncü Resmi">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-4">Ürün İndirim</label>
+                        <input type="text" class="form-control" id="input-4" placeholder="Örnek Yüzde 10 İndirim İçin 10 Yazınız">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-4">Ürün Kategori</label>
+                        <input type="text" class="form-control" id="input-4" placeholder="Ürün Kategori Giriniz">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-4">Ürün AltKategorisi</label>
+                        <input type="text" class="form-control" id="input-4" placeholder="Ürün AltKategorisi Giriniz">
+                     </div>
+                     <div class="form-group">
+                        <label for="input-4">Ürün Açıklama</label>
+                        <textarea name="" id="" cols="30" rows="10" placeholder="HTML Kullanabilirsiniz"></textarea>
+                     </div>
+                     <div class="form-group">
+                        <button type="submit" class="btn btn-light px-5"><i class="icon-lock"></i> Kaydet</button>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="overlay toggle-menu"></div>
+      <!--end overlay-->
+   </div>
+   <!-- End container-fluid-->
+</div>
